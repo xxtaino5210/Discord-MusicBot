e89c2db7239db51f9b500070afc6c2ed82be6c1c
xxtaino5210 <42452904+xxtaino5210@users.noreply.github.com>
2021-07-26 23:05:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ module.exports = {
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
   CookieSecret: "Pikachu is cute", //A Secret like a password
+toKEN : PROCEVSS.ENV.TOKENM | // DEFULT TOKEN 
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
